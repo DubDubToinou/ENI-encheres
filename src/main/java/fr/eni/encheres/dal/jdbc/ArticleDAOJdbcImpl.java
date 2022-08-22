a33c1c0af6814346391f14b1e9051fa3a0109c0a
FrancoisFarlan <francois.farlan@gmail.com>
2022-08-22 18:21:37 +0200

    travail articledao

--- src/main/java/fr/eni/encheres/dal/jdbc/ArticleDAOJdbcImpl.java
+++ src/main/java/fr/eni/encheres/dal/jdbc/ArticleDAOJdbcImpl.java
@@ -6,6 +6,7 @@ import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.ArticleDAO;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleDAOJdbcImpl implements ArticleDAO {
@@ -15,7 +16,15 @@ public class ArticleDAOJdbcImpl implements ArticleDAO {
     private static final String INSERT_RETRAIT = "INSERT INTO RETRAITS(no_article, rue, code_postal, ville VALUES (?, ?, ?, ?)";
     private static final String UPDATE_ARTICLE = "UPDATE Articles SET nom_article = ?, description = ?, date_debut_encheres = ?, " +
             "date_fin_encheres = ?, prix_initial = ?, no_categorie = ? WHERE no_article = ?";
+    private static final String UPDATE_RETRAIT = "UPDATE Retraits SET rue = ?, code_postal = ?, ville = ? WHERE no_article = ?";
     private static final String DELETE_ARTICLE = "DELETE FROM Articles WHERE no_article = ?";
+    private static final String SELECT_ALL = "SELECT a.no_article, a.nom_article, a.description, a.date_debut_encheres, a.date_fin_encheres, " +
+            "a.prix_initial, a.prix_vente, u.pseudo, c.libelle FROM Articles a" +
+            "LEFT JOIN Utilisateurs u " +
+            "ON a.no_utilisateur = u.no_utilisateur" +
+            "LEFT JOIN Categories c " +
+            "ON a.no_categorie = c.no_categorie";
+
 
     @Override
     public void insert(Articles article) {
@@ -24,9 +33,11 @@ public class ArticleDAOJdbcImpl implements ArticleDAO {
         }
 
         try(Connection con = ConnectionProvider.getConnection()) {
+
             PreparedStatement pstmtArticle = con.prepareStatement(INSERT_ARTICLE, PreparedStatement.RETURN_GENERATED_KEYS);
             PreparedStatement pstmtRetrait = con.prepareStatement(INSERT_RETRAIT);
 
+            //Ajout de l'article
             pstmtArticle.setString(1, article.getNomArticle());
             pstmtArticle.setString(2, article.getDescription());
             pstmtArticle.setDate(3, Date.valueOf(article.getDateDebutEncheres()));
@@ -43,6 +54,7 @@ public class ArticleDAOJdbcImpl implements ArticleDAO {
             if(rs.next()) {
                 article.setNoArticle(rs.getInt("no_article"));
 
+                //ajout du lieu de retrait (par défaut adresse du vendeur)
                 pstmtRetrait.setInt(1, article.getNoArticle());
                 pstmtRetrait.setString(2, article.getUtilisateurs().getRue());
                 pstmtRetrait.setString(3, article.getUtilisateurs().getCodePostal());
@@ -54,26 +66,71 @@ public class ArticleDAOJdbcImpl implements ArticleDAO {
             rs.close();
             pstmtArticle.close();
             pstmtRetrait.close();
-            con.close();
 
+
+        } catch(SQLException ex) {
+//TODO
+        }
+    }
+
+    @Override
+    public void update(Articles article) {
+
+        try(Connection con = ConnectionProvider.getConnection()) {
+
+            PreparedStatement pstmtArticle = con.prepareStatement(UPDATE_ARTICLE);
+            PreparedStatement pstmtRetrait = con.prepareStatement(UPDATE_RETRAIT);
+
+            pstmtArticle.setString(1, article.getNomArticle());
+            pstmtArticle.setString(2, article.getDescription());
+            pstmtArticle.setDate(3, Date.valueOf(article.getDateDebutEncheres()));
+            pstmtArticle.setDate(4, Date.valueOf(article.getDateFinEncheres()));
+            pstmtArticle.setInt(5, article.getMiseAPrix());
+            pstmtArticle.setInt(6, article.getCategorieArticle().getNoCategorie());
+            pstmtArticle.setInt(7, article.getNoArticle());
+
+            pstmtArticle.executeUpdate();
+
+            pstmtRetrait.setString(1, article.getLieuRetrait().getRue());
+            pstmtRetrait.setString(2, article.getLieuRetrait().getCodePostal());
+            pstmtRetrait.setString(3, article.getLieuRetrait().getVille());
+            pstmtRetrait.setInt(4, article.getNoArticle());
+
+            pstmtArticle.close();
+            pstmtRetrait.close();
+
+        } catch (SQLException ex) {
+    //TODO
+        }
+    }
+
+    @Override
+    public void delete(Articles article) {
+
+        try(Connection con = ConnectionProvider.getConnection()) {
+            PreparedStatement pstmtArticle = con.prepareStatement(DELETE_ARTICLE);
+
+
+            pstmtArticle.setInt(1, article.getNoArticle());
+            pstmtArticle.executeUpdate();
+
+            pstmtArticle.close();
 
         } catch(SQLException ex) {
 
         }
     }
-
-    @Override
-    public void update(Articles articles) {
-
-    }
-
-    @Override
-    public void delete(Articles articles) {
-
-    }
     @Override
     public List<Articles> selectAll() {
-        return null;
+        List<Articles> listeArticles = new ArrayList<>();
+
+        try (Connection con = ConnectionProvider.getConnection()){
+
+        } catch(SQLException ex) {
+            //TODO
+        }
+
+        return listeArticles;
     }
 
     @Override
