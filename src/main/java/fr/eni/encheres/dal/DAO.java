24b2e82c4c97fd6f353aa612c0bcc1708439b4c2
FrancoisFarlan <francois.farlan@gmail.com>
2022-08-22 14:09:21 +0200

    dao

---
+++ src/main/java/fr/eni/encheres/dal/DAO.java
@@ -0,0 +1,7 @@
+package fr.eni.encheres.dal;
+
+import java.util.List;
+
+public interface DAO<T> {
+
+}
--- src/main/java/fr/eni/encheres/dal/DAOFactory.java
+++ src/main/java/fr/eni/encheres/dal/DAOFactory.java
@@ -1,5 +1,8 @@
 package fr.eni.encheres.dal;
 
+import fr.eni.encheres.dal.jdbc.ArticleDAOJdbcImpl;
+import fr.eni.encheres.dal.jdbc.UtilisateurDAOJdbcImpl;
+
 public abstract class DAOFactory {
 
     public static UtilisateurDAO getUtilisateurDAO() {
