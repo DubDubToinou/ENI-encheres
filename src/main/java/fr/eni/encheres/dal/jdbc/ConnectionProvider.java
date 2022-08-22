40dc6f94083d26c72a958ba8c841bf3f358e2c93
DubDubToinou <dubuc.ant@gmail.com>
2022-08-22 10:52:25 +0200

    test

--- src/main/java/fr/eni/encheres/dal/jdbc/ConnectionProvider.java
+++ src/main/java/fr/eni/encheres/dal/jdbc/ConnectionProvider.java
@@ -1,4 +1,25 @@
-package fr.eni.encheres.dal.jdbc;
+package fr.eni.tpjee.mesapplications.repas.dal.jdbc;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionProvider {
+
+    private static javax.sql.DataSource dataSource;
+
+    static {
+
+        try {
+            dataSource = InitialContext.doLookup("java:comp/env/jdbc/pool_cnx");
+
+        } catch (NamingException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
 }
