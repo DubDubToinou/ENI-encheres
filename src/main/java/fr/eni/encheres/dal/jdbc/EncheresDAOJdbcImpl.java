055a03cf2066a260ecd95b618902729b49db8af8
DubDubToinou <dubuc.ant@gmail.com>
2022-08-22 17:06:59 +0200

    Ajout EncheresDAOJdbcImpl

--- src/main/java/fr/eni/encheres/dal/jdbc/EncheresDAOJdbcImpl.java
+++ src/main/java/fr/eni/encheres/dal/jdbc/EncheresDAOJdbcImpl.java
@@ -1,6 +1,53 @@
 package fr.eni.encheres.dal.jdbc;
 
+/**
+ * @Author Antoine
+ */
+
+import fr.eni.encheres.bo.Enchere;
 import fr.eni.encheres.dal.EncheresDAO;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class EncheresDAOJdbcImpl implements EncheresDAO {
+
+
+    public Enchere selectByNumArticle(int idArticle) throws SQLException {
+
+        String sqlSelectArticle = ("SELECT NO");
+
+        try (Connection con = ConnectionProvider.getConnection()) {
+
+            PreparedStatement stmt = con.prepareStatement("");
+
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void insert(Enchere elementEnchere){
+
+        String  sqlInsertEnchere = ("INSERT INTO Encheres (no_utilisateur, no_article, date_enchere, montant_enchere) VALUES = (?, ?, ?, ?)");
+
+        try(Connection con = ConnectionProvider.getConnection()){
+
+        PreparedStatement stmt = con.prepareStatement(sqlInsertEnchere);
+
+        stmt.setInt(1, elementEnchere.getUtilisateur().getNoUtilisateur());
+        stmt.setInt(2, elementEnchere.getArticleVendu().getNoArticle());
+        stmt.setDate(3, Date.valueOf(elementEnchere.getDateEnchere()));
+        stmt.setInt(4, elementEnchere.getMontant_enchere());
+
+
+        }catch(SQLException ex){
+            ex.printStackTrace();
+
+        }
+    }
 }
