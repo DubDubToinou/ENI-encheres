457c07e99b356b2264e4f9bf78f29ffa800c098b
DubDubToinou <dubuc.ant@gmail.com>
2022-08-23 17:09:29 +0200

    Ajout CategorieDAO + jdbc impl

--- src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
@@ -1,7 +1,10 @@
 package fr.eni.encheres.bll;
 
+import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.DAOFactory;
 import fr.eni.encheres.dal.UtilisateurDAO;
+
+import java.sql.SQLException;
 
 public class UtilisateurManager {
 
@@ -9,6 +12,14 @@ public class UtilisateurManager {
 
     public UtilisateurManager() {
         this.utilisateurDAO = DAOFactory.getUtilisateurDAO();
+    }
+
+    public Utilisateur afficherSonProfil(String pseudo) throws SQLException{
+        return this.utilisateurDAO.selectByID(pseudo);
+    }
+
+    public Utilisateur afficherUnProfil(String pseudo) throws SQLException{
+        return this.utilisateurDAO.selectOwnProfile(pseudo);
     }
 
 }
