41964e19ae2faaf093619ea5bc973d3b3c5fecbe
DubDubToinou <dubuc.ant@gmail.com>
2022-08-24 11:35:39 +0200

    Ajout CategorieDAO + jdbc impl

---
+++ src/main/java/fr/eni/encheres/bll/CodesResultatBLL.java
@@ -0,0 +1,6 @@
+package fr.eni.encheres.bll;
+
+public abstract class CodesResultatBLL {
+
+    public static final int REGLE_USER_PSEUDO_ERREUR = 20000;
+}
--- src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
@@ -1,5 +1,6 @@
 package fr.eni.encheres.bll;
 
+import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.DAOFactory;
 import fr.eni.encheres.dal.UtilisateurDAO;
@@ -15,12 +16,30 @@ public class UtilisateurManager {
     }
 
 
+    public void removeUser(Utilisateur utilisateur) throws SQLException {
+
+        utilisateurDAO.delete(utilisateur);
+
+    }
+
     public Utilisateur afficherSonProfil(String pseudo) throws SQLException{
         return this.utilisateurDAO.selectByPseudo(pseudo);
     }
 
     public Utilisateur afficherUnProfil(String pseudo) throws SQLException{
         return this.utilisateurDAO.selectOwnProfile(pseudo);
+    }
+
+    public void validateUser(Utilisateur utilisateur, BusinessException businessException){
+
+        if (utilisateur.getPseudo() == null || utilisateur.getPseudo().isBlank()){
+            businessException.ajouterErreur(CodesResultatBLL.REGLE_USER_PSEUDO_ERREUR);
+        }
+
+        if(utilisateur.getNom() == null || utilisateur.getNom().isBlank()){
+            businessException.ajouterErreur(CodesResultatBLL);
+        }
+
     }
 
 }
