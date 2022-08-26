28235ea6bda8ded83d85c8c2dfabbfc0f92c0623
DubDubToinou <dubuc.ant@gmail.com>
2022-08-26 11:03:26 +0200

    Ajout profile. en cours modifierProfil.jsp

--- src/main/java/fr/eni/encheres/servlets/NouvelleVenteServlet.java
+++ src/main/java/fr/eni/encheres/servlets/NouvelleVenteServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.util.List;
-  /*
+
 @WebServlet(value = "/NouvelleVente")
 public class NouvelleVenteServlet extends HttpServlet {
     @Override
@@ -128,4 +128,4 @@ public class NouvelleVenteServlet extends HttpServlet {
         Retrait lieuRetrait = new Retrait(rue, codePostal, ville);
         return lieuRetrait;
     }
-}                   */
+}
